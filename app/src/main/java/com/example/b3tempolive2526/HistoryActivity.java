72038ded96f299c4caedaa6a39ec59661424a165
lpastyle <lpastorelli@lpastyle.com>
2026-06-25 12:31:09 +0200

    Added tempo date adapter template

--- app/src/main/java/com/example/b3tempolive2526/HistoryActivity.java
+++ app/src/main/java/com/example/b3tempolive2526/HistoryActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.b3tempolive2526.databinding.ActivityHistoryBinding;
 import com.example.b3tempolive2526.model.TempoDate;
@@ -32,6 +33,12 @@ public class HistoryActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Init view
+        binding.tempoHistoryRv.setHasFixedSize(true);
+        binding.tempoHistoryRv.setLayoutManager(new LinearLayoutManager(this));
+        TempoDateAdapter tempoDateAdapter = new TempoDateAdapter();
+        binding.tempoHistoryRv.setAdapter(tempoDateAdapter);
 
         ArrayList<TempoDate> parcelableArrayListExtra = getIntent().getParcelableArrayListExtra(MainActivity.TEMPO_CALENDAR_EXTRA_KEY);
         if (parcelableArrayListExtra != null) {
