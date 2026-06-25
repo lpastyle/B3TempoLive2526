e87574029db1d5d2290d36f96511561463092192
lpastyle <lpastorelli@lpastyle.com>
2026-06-25 12:39:55 +0200

    Added tempo date view holder template

--- app/src/main/java/com/example/b3tempolive2526/TempoDateAdapter.java
+++ app/src/main/java/com/example/b3tempolive2526/TempoDateAdapter.java
@@ -1,24 +1,32 @@
 package com.example.b3tempolive2526;
 
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TempoDateAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class TempoDateAdapter extends RecyclerView.Adapter<TempoDateAdapter.TempoDateViewHolder> {
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TempoDateAdapter.TempoDateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TempoDateAdapter.TempoDateViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public static class TempoDateViewHolder extends RecyclerView.ViewHolder {
+
+        public TempoDateViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
